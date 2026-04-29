afa6f28f7f705a1919150cf69856713d8e667512
Hrushant Motinge <155289678+Hrushant1909@users.noreply.github.com>
2026-04-29 11:15:53 +0530

    Fixing the start and stop .sh  files

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "Stopping existing Spring Boot application..."
+echo "Stopping existing applications..."
 
 pkill -f buildright-app.jar || true
+pkill -f java || true
+pkill -f python || true
+pkill -f node || true
+
+sleep 10
